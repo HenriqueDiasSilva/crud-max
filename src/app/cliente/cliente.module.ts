d102c302df6a20c6e03bb789950fbc21f66c2960
HenriqueDiasSilva <henriquediasilv1@gmail.com>
2024-08-01 17:18:59 -0300

    feat: Adiciona tooltip aos ícones e validação se tem informação na listagem de clientes

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { ClienteRoutingModule } from "./cliente-routing.module";
-import { ClienteComponent } from "./cliente.component";
-import { AddComponent } from "./add/add.component";
-import { EditComponent } from "./edit/edit.component";
-import { DeleteComponent } from "./delete/delete.component";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatCardModule } from "@angular/material/card";
-import { MatInputModule } from "@angular/material/input";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatSelectModule } from "@angular/material/select";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatTableModule } from "@angular/material/table";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatNativeDateModule, MAT_DATE_LOCALE  } from "@angular/material/core";
+import { ClienteRoutingModule } from './cliente-routing.module';
+import { ClienteComponent } from './cliente.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { DeleteComponent } from './delete/delete.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -38,10 +39,9 @@ import { FormsModule } from '@angular/forms';
     MatTableModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    FormsModule
+    FormsModule,
+    MatTooltipModule,
   ],
-  providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }
-  ],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }],
 })
 export class ClienteModule {}
