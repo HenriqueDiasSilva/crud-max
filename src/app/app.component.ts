749ecd1a013d422e228b1c352efe636802eadaf1
HenriqueDiasSilva <henriquediasilv1@gmail.com>
2024-08-01 01:56:26 -0300

    feat: Adiciona roteamento, responsividade e modificação no título com base na página atual no componente/módulo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+import { ClienteService } from "./services/cliente.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'Clientes';
-  urlLogo = 'https://desenvolvimento.maxdata.com.br/assets/imagens/login/MaxWEB.svg'
-
+export class AppComponent implements OnInit {
+  title = "Clientes";
+  urlLogo = "https://desenvolvimento.maxdata.com.br/assets/imagens/login/MaxWEB.svg";
   sidebarOpen = false;
-  
-  toggleSidebar(){
+
+  private user_login = {
+    email: "usuarioteste@maxdata.com.br",
+    senha: "j>grr@je",
+  };
+
+  constructor(private router: Router, private clienteService: ClienteService) {
+    this.clienteService.login(this.user_login);
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.title = this.getTitleFromRoute(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  getTitleFromRoute(url: string): string {
+    if (url.includes("add")) {
+      return "Novo Cliente";
+    } else if (url.includes("edit")) {
+      return "Cliente";
+    } else {
+      return "Clientes";
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +8,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -22,9 +22,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatSidenavModule,
     MatListModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule, 
+    HttpClientModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
