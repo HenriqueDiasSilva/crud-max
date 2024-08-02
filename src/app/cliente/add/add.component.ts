1e38e753f903801a3138e73b230dcadfc30841e8
HenriqueDiasSilva <henriquediasilv1@gmail.com>
2024-08-02 18:33:09 -0300

    feat: Adiciona integração com API para criação de cliente

--- src/app/cliente/add/add.component.ts
+++ src/app/cliente/add/add.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { ClienteService } from 'src/app/services/cliente.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 interface Value {
   value: string;
@@ -15,30 +16,29 @@ interface Value {
 export class AddComponent {
   isDisabled: boolean = true;
 
-  id: number = 0;
-
-  register: string = '';
-  person: string = '';
-  client_name: string = '';
-  client_cpf_cnpj: string = '';
-  active: string = '';
+  client_register: string | null = null;
+  client_person: string | null = null;
+  client_name: string | null = null;
+  client_cpf_cnpj: string | null = null;
+  client_active: string | null = null;
 
   client_type: string = '';
-  client_fantasy: string = '';
-  alter: string = '';
-  client_rg: string = '';
-  client_phone: string = '';
-  client_cell_phone: string = '';
-  discount: number = 0;
+  client_fantasy: string | null = null;
+  client_alter: string | null = null;
+  client_rg: string | null = null;
+  client_phone: string | null = null;
+  client_cell_phone: string | null = null;
+  client_discount: number = 0;
 
-  cep: string = '';
-  uf: string = '';
-  county: string = '';
-  adress: string = '';
-  neighborhood: string = '';
-  number: string = '';
-  ie: string = '';
-  description: string = '';
+  adress_cep: string | null = null;
+  adress_uf: string | null = null;
+  adress_county: string | null = null;
+  adress: string | null = null;
+  adress_neighborhood: string | null = null;
+  adress_number: string | null = null;
+  adress_ie: string | null = null;
+  adress_description: string | null = null;
+  adress_ibge: string = '';
 
   registers: Value[] = [
     { value: 'Cliente', view_value: 'Cliente' },
@@ -69,21 +69,125 @@ export class AddComponent {
     { value: '3', view_value: 'Produtor Rural' },
   ];
 
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action);
+  }
+
   constructor(
     private clienteService: ClienteService,
-    private route: ActivatedRoute
+    private snackBar: MatSnackBar,
+    private router: Router,
   ) {}
 
   searchCEP() {
-    this.clienteService.openCEP(this.cep).subscribe((response: any) => {
-      this.neighborhood = response.bairro;
-      this.cep = response.cep;
-      this.description = response.complemento;
-      this.county = response.localidade;
+    this.clienteService.openCEP(this.adress_cep).subscribe((response: any) => {
+      this.adress_neighborhood = response.bairro;
+      this.adress_description = response.complemento;
+      this.adress_county = response.localidade;
       this.adress = response.logradouro;
-      this.uf = response.uf;
+      this.adress_uf = response.uf;
+      this.adress_ibge = response.ibge;
 
       this.isDisabled = false;
+    });
+  }
+
+  salvar() {
+    if (!this.client_name || !this.client_cpf_cnpj || !this.adress_cep || !this.adress || !this.adress_neighborhood || !this.adress_number) {
+      this.openSnackBar('Preencha todos os campos obrigatórios', 'Fechar');
+      return;
+    }
+
+    let data = {
+      nome: this.client_name,
+      fantasia: this.client_fantasy,
+      tipo_pessoa: this.client_person,
+      tipo_cadastro: this.client_register,
+      cadastro_grupo_id: null,
+      cadastro_tipo_id: parseInt(this.client_type),
+      cadastro_profissao_id: null,
+      cpf_cnpj: this.client_cpf_cnpj,
+      rg_ie: this.client_rg,
+      rg_ie_uf: null,
+      ie_diferido: null,
+      dt_nascimento: null,
+      vlr_limite_credito: null,
+      obs_venda: null,
+      fone: this.client_phone,
+      fax: null,
+      celular: this.client_cell_phone,
+      site: null,
+      email: null,
+      sexo: null,
+      estado_civil: null,
+      naturalidade_cidade: null,
+      naturalidade_uf: null,
+      nome_pai: null,
+      nome_mae: null,
+      qtd_dependentes: null,
+      dados_prof_nome_empresa: null,
+      dados_prof_cnpj: null,
+      dados_prof_fone: null,
+      dados_prof_data_admissao: null,
+      dados_prof_ocupacao: null,
+      dados_prof_cargo: null,
+      dados_prof_vlr_renda_mensal: null,
+      dados_prof_vlr_outras_rendas: null,
+      dados_prof_endereco: null,
+      dados_prof_endereco_numero: null,
+      dados_prof_endereco_bairro: null,
+      dados_prof_endereco_cep: null,
+      dados_prof_endereco_municipio_codigo_ibge: null,
+      dados_banc_num_banco: null,
+      dados_banc_nome_banco: null,
+      dados_banc_agencia: null,
+      dados_banc_num_conta: null,
+      dados_banc_tipo_conta: null,
+      dados_banc_data_conta: null,
+      dados_banc_fone_ag: null,
+      dados_banc_obs: null,
+      obs_geral: null,
+      tipo_regime_apuracao: null,
+      nome_conjuge: null,
+      inscricao_municipal: null,
+      dt_casamento: null,
+      id_print_wayy: null,
+      emp_id: 1,
+      chk_emp_disponivel: true,
+      chk_alterar_nome: Boolean(this.client_alter),
+      desconto_auto_aplicar: false,
+      desconto_auto_aliq: null,
+      obs_nfe: null,
+      consumidor_final: false,
+      tipo_preco_venda: null,
+      cadastro_empresa_id: null,
+      cadastro_empresa_guid: null,
+      ativo: Boolean(this.client_active),
+      dt_ultima_alteracao: null,
+      usuario_ultima_alteracao_id: null,
+      usuario_ultima_alteracao_nome: null,
+      dt_inclusao: null,
+      usuario_inclusao_id: null,
+      guid: null,
+      cadastro_endereco_padrao: {
+        descricao: this.adress_description,
+        ativo: true,
+        endereco: this.adress,
+        endereco_numero: this.adress_number,
+        endereco_bairro: this.adress_neighborhood,
+        endereco_cep: this.adress_cep,
+        endereco_municipio_codigo_ibge: parseInt(this.adress_ibge),
+        principal: true,
+        cobranca: false,
+        ie_produtor_rural: this.adress_ie,
+      },
+    };
+
+    this.clienteService.createClient(data).subscribe((response: any) => {
+      if (response) {
+        this.openSnackBar('Cliente criado com sucesso', 'Fechar');
+        this.router.navigate(['/clientes']);
+      }
     });
   }
 }
